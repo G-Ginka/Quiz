e400f6396fbfdf14fd335956389296e40ac504c2
G-Ginka <70010377+G-Ginka@users.noreply.github.com>
2020-08-21 15:18:31 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,52 @@
+
+
+//Array of objects
+const quiz = [
+{
+	q:'What does HTML stand for?',
+	options:['Home Tool Markup Language','Hyperlinks and Text Markup Language','Hyper Text Markup Language'],
+	answer:2
+},
+{
+	q:'What is the correct HTML element for inserting a line break?',
+	options:['lb','br','break'],
+	answer:1		
+},
+{
+	q:'Which character is used to indicate an end tag?',
+	options:['/','*','<','^'],
+	answer:0		
+},
+{
+	q:'What does CSS stand for ?',
+	options:['Colorful Style Sheets','Computing Style Sheets','Cascading Style Sheets','Creative Style Sheets'],
+	answer:2		
+},
+{
+	q:'Which HTML tag is used to define an internal style sheet?',
+	options:['Script tag','Style tag','Css tag'],
+	answer:1		
+},
+{
+	q:'Which property is used to change the background color?',
+	options:['color','background-color','bgcolor'],
+	answer:1		
+},
+	{
+	q:'Where is the correct place to insert a javascript?',
+	options:['head section','body section','Both head and body section'],
+	answer:2		
+},
+	{
+	q:'The external javascript file must contain the script tag?',
+	options:['True','False'],
+	answer:1		
+},
+	{
+	q:'How do you round the number 7.25, to the nearest integer?',
+	options:['Math.round(7.25)','Math.rnd(7.25)','round(7.25)','rnd(7.25)'],
+	answer:0		
+},
+	]
+		
+	  
